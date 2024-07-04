1e4611de08ca6aa586c3cb31d7992f7ba2f05858
Kristijannm <119734900+Kristijannm@users.noreply.github.com>
2024-07-04 13:55:07 +0200

    homePage added

---
+++ scripts/scriptInitial.js
@@ -0,0 +1,43 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  let currentPage = 1;
+  let pageSize = 24;
+
+
+  function handleNavClick(event) {
+    event.preventDefault();
+
+    let navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+    });
+
+    this.classList.add('active');
+
+    let target = this.getAttribute('data-target');
+    let pageSections = document.querySelectorAll('.page-section');
+
+    pageSections.forEach(section => {
+      section.classList.remove('active');
+    });
+
+    document.getElementById(target).classList.add('active');
+
+
+    if (target === 'categoryPage') {
+      loadCategoryData();
+    }
+  }
+
+  let navLinks = document.querySelectorAll('.nav-link');
+  
+  navLinks.forEach(link => {
+    link.addEventListener('click', handleNavClick);
+  })
+});
+
+document.querySelector('.nav-link[data-target="homePage"]').classList.add('active');
+document.getElementById('homePage').classList.add('active');
