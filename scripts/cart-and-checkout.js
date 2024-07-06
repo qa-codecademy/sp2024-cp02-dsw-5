0850351e935dd7dbdb444985bfabd259cb6aad4d
Jana <jana.guguloska@gmail.com>
2024-07-06 18:33:33 +0200

    latest changes Jana

---
+++ scripts/cart-and-checkout.js
@@ -0,0 +1,180 @@
+let orderSuccessMessage = document.getElementById("orderSuccessMessage");
+
+let products = localStorage.getItem("cartItems");
+
+if (products) {
+    products = JSON.parse(products);
+  } else {
+    products = [];
+  }
+
+//--------------------------
+
+document.addEventListener('DOMContentLoaded', LoadCartProducts(products));
+
+function LoadCartProducts(products) {
+    const cartContainer = document.getElementById('cart-container');
+    let emptyCartMessage = document.getElementById('emptyCartMessage');
+    // Function to render products in the cart
+    const renderCartItems = () => {
+        cartContainer.innerHTML = "";
+        if (products.length === 0) {
+            emptyCartMessage.style.display = 'block';
+        } else {
+            emptyCartMessage.style.display = 'none';
+        }
+        products.forEach(product => {
+            const cartItem = document.createElement('div');
+            cartItem.className = 'row cart-item';
+            cartItem.dataset.price = product.price;
+
+            cartItem.innerHTML = `
+                <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                    <div class="bg-image ripple rounded" data-mdb-ripple-color="light">
+                        <img class="productImg w-100" src="${product.img}" alt="${product.name}" />
+                        <a href="#!">
+                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)"></div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                    <p class="productName">${product.name}</p>
+                    <p class="productCategory">Category: ${product.category}</p>
+                    <p class="productDiscount">${product.onSale ? `Discount: ${product.discount}%` : '<br/>'}</p>
+                    <p class="data-price">Price: $${product.price}</p>
+
+                    <button type="button" class="btn btn-primary btn-sm me-1 mb-2 remove-item" data-product-id="${product.id}" data-mdb-tooltip-init title="Remove item">
+                        <i class="fas fa-trash"></i>Remove
+                    </button>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                    <div class="d-flex mb-4" style="max-width: 300px">
+                        <button class="btn btn-primary px-3 me-2 btn-minus">
+                            <i class="fas fa-minus"></i> 
+                        </button>
+                        <div data-mdb-input-init class="form-outline">
+                            <input min="1" name="quantity" value="1" type="number" class="form-control quantity" />
+                            <label class="form-label" for="form1">Quantity</label>
+                        </div>
+                        <button class="btn btn-primary px-3 ms-2 btn-plus">
+                            <i class="fas fa-plus"></i> 
+                        </button>
+                    </div>
+                    <p class="text-start text-md-center item-total-price">$${product.onSale ? (product.price*(100-product.discount)/100) : product.price}</p>
+                </div>
+            
+            `;
+
+            cartContainer.appendChild(cartItem);
+        });
+        addEventListeners();
+        updateTotalPrice();
+    };
+
+    const discountRate = 0.1; // 10% discount
+
+    const updateTotalPrice = () => {
+        const cartItems = document.querySelectorAll('.cart-item');
+        let totalPrice = 0;
+
+        cartItems.forEach(item => {
+            const quantity = parseInt(item.querySelector('.quantity').value);
+            const price = parseFloat(item.dataset.price);
+            const itemTotalPrice = quantity * price;
+            item.querySelector('.item-total-price').textContent = `$${itemTotalPrice.toFixed(2)}`;
+            totalPrice += itemTotalPrice;
+        });
+
+        const discount = totalPrice * discountRate;
+        const totalAmount = totalPrice - discount;
+
+        document.getElementById('total-price').textContent = `$${totalPrice.toFixed(2)}`;
+        document.getElementById('total-amount').textContent = `$${totalAmount.toFixed(2)}`;
+    };
+
+    const addEventListeners = () => {
+        document.querySelectorAll('.btn-plus').forEach(btn => {
+            btn.addEventListener('click', function () {
+                const quantityInput = this.closest('.d-flex').querySelector('.quantity');
+                quantityInput.stepUp();
+                updateTotalPrice();
+            });
+        });
+
+        document.querySelectorAll('.btn-minus').forEach(btn => {
+            btn.addEventListener('click', function () {
+                const quantityInput = this.closest('.d-flex').querySelector('.quantity');
+                quantityInput.stepDown();
+                updateTotalPrice();
+            });
+        });
+
+        document.querySelectorAll('.remove-item').forEach(btn => {
+            btn.addEventListener('click', function () {
+                const productId = this.getAttribute("data-product-id");
+                //this.closest('.cart-item').remove();
+                removeProductFromCart(productId);
+                updateTotalPrice();
+            });
+        });
+
+        
+    };
+
+    renderCartItems(products);
+
+    
+//CHECKOUT
+
+
+    const btnCheckout = document.getElementById("goToCheckout");
+    function checkout() {
+        const checkoutAlert = document.getElementById("checkoutAlert");
+        const totalAmount = parseFloat(document.getElementById("total-amount").textContent.substring(1));
+        if (totalAmount <= 0) {
+            checkoutAlert.innerText = "Your cart is empty. Add items to cart!";
+        } else {
+            checkoutAlert.innerText = "Choose your payment method and shipping address to complete your order!";
+        }
+    };
+
+    btnCheckout.addEventListener("click", checkout);
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const orderForm = document.getElementById('orderForm');
+
+    orderForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' 
+          });
+        if (orderForm.checkValidity() ) {
+            //alert('The order is successfully submitted.');
+            orderSuccessMessage.innerText = "The order is successfully submitted.";
+            setTimeout(() => {
+                orderSuccessMessage.innerHTML = ""; 
+              }, 4000);
+            localStorage.setItem("cartItems",JSON.stringify([]));
+            let jsondata = localStorage.getItem("cartItems");
+            products = JSON.parse(jsondata);
+            LoadCartProducts(products);
+            
+        } else {
+            // Highlight the fields with errors
+            orderForm.reportValidity() ;
+            alert('Your cart is empty! Add products to submit your order!');
+        }
+    });
+});
+
+function removeProductFromCart(productId) {
+    products = products.filter(product => product.id !== productId);
+    localStorage.setItem("cartItems", JSON.stringify(products));
+    LoadCartProducts(products);
+}
+
+
---
+++ scripts/category.js
@@ -0,0 +1,228 @@
+async function loadCategoryData() {
+  const categoriesUrl = "./assets/categories.json";
+  const productsUrl = "./assets/products.json";
+
+  try {
+    const [categoriesResponse, productsResponse] = await Promise.all([
+      fetch(categoriesUrl),
+      fetch(productsUrl),
+    ]);
+
+    const categoriesData = await categoriesResponse.json();
+    const productsData = await productsResponse.json();
+
+    populateCategories(categoriesData.categories);
+
+    updatePageSize();
+
+    setupPagination(productsData);
+
+    document.getElementById("categories").addEventListener("change", function () {
+      currentPage = 1;
+      filterProducts(); 
+    });
+    
+    document.getElementById("materials").addEventListener("change", function () {
+      currentPage = 1;
+      filterProducts();
+    });
+    
+    document.getElementById("pageSize").addEventListener("change", function () {
+      currentPage = 1;
+      updatePageSize();
+    });
+    
+    document.getElementById("priceRange").addEventListener("input", function () {
+      currentPage = 1; 
+      filterProducts();
+    });
+} catch (error) {
+  console.error("Error loading data:", error);
+}
+}
+
+async function loadSalesData() {
+  const productsUrl = './assets/products.json';
+
+  try {
+    const productsResponse = await fetch(productsUrl);
+    const productsData = await productsResponse.json();
+
+    const onSaleProducts = productsData.filter(product => product.onSale);
+    populateSalesProducts(onSaleProducts);
+
+  } catch (error) {
+    console.error('Error loading sales data:', error);
+  }
+}
+
+function populateCategories(categories) {
+  const categoriesSelect = document.getElementById("categories");
+  categoriesSelect.innerHTML = '<option value="all">All</option>';
+
+  categories.forEach((category) => {
+    const option = document.createElement("option");
+    option.value = category.name;
+    option.textContent = category.name;
+    categoriesSelect.appendChild(option);
+  });
+}
+
+function populateProducts(products) {
+  const productList = document.getElementById("productList");
+  productList.innerHTML = "";
+
+  let startIndex = (currentPage - 1) * pageSize;
+  let endIndex = currentPage * pageSize;
+
+  products.slice(startIndex, endIndex).forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.className =
+      "card m-2 col-sm-6 col-md-4 col-lg-3 position-relative";
+    productDiv.innerHTML = `
+        <img src="${product.img}" class="card-img-top" alt="${product.name}">
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title">${product.name}</h5>
+          <p class="card-text">${product.material}</p>
+          <p class="card-description flex-grow-1 hide-on-small-screen">${product.description}</p>
+          <div class="mt-auto">
+            <p class="card-text" style="font-size: 1.5em;">$${product.price}</p>
+            <button class="btn btn-primary add-to-cart-btn mt-3 addToCartBtn" data-product-id="${product.id}">Add to Cart</button>
+          </div>
+        </div>
+      `;
+    productList.appendChild(productDiv);
+  });
+
+  setupPagination(products);
+
+  attachAddToCartListeners();
+}
+//Function to handle the sales page products display
+function populateSalesProducts(products) {
+  const salesProductList = document.getElementById('salesProductList');
+  salesProductList.innerHTML = '';
+
+  products.forEach(product => {
+
+    const productDiv = document.createElement('div');
+    productDiv.className = 'card m-2 col-sm-6 col-md-4 col-lg-3 position-relative';
+    productDiv.innerHTML = `
+      <img src="${product.img}" class="card-img-top" alt="${product.name}">
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title">${product.name}</h5>
+        <p class="card-text">${product.material}</p>
+        <p class="card-description flex-grow-1 hide-on-small-screen">${product.description}</p>
+        <div class="mt-auto">
+          <p class="card-text" style="font-size: 1.5em;">$${product.price}</p>
+          <button class="btn btn-primary add-to-cart-btn mt-3 addToCartBtn" data-product-id="${product.id}">Add to Cart</button>
+        </div>
+      </div>
+    `;
+    salesProductList.appendChild(productDiv);
+  });
+  attachAddToCartListeners();
+}
+
+
+function filterProducts() {
+  const selectedCategory = document.getElementById("categories").value;
+  const selectedMaterial = document.getElementById("materials").value;
+  const priceRange = document.getElementById("priceRange").value;
+  const productsUrl = "assets/products.json";
+
+  fetch(productsUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let filteredProducts = data.filter((product) => {
+        if (
+          selectedCategory !== "all" &&
+          product.category !== selectedCategory
+        ) {
+          return false;
+        }
+        if (
+          selectedMaterial !== "all" &&
+          product.material.toLowerCase() !== selectedMaterial.toLowerCase()
+        ) {
+          return false;
+        }
+        if (product.price > priceRange) {
+          return false;
+        }
+        return true;
+      });
+
+      populateProducts(filteredProducts);
+
+      document.getElementById(
+        "priceRangeValue"
+      ).textContent = `$0 - $${priceRange}`;
+    })
+
+    .catch((error) => console.error("Error filtering products:", error));
+}
+
+function updatePageSize() {
+  pageSize = parseInt(document.getElementById("pageSize").value);
+  currentPage = 1;
+  filterProducts();
+}
+
+function setupPagination(products) {
+  const paginationElement = document.getElementById("pagination");
+  paginationElement.innerHTML = "";
+
+  const pageCount = Math.ceil(products.length / pageSize);
+
+  for (let i = 1; i <= pageCount; i++) {
+    const li = document.createElement("li");
+    li.classList.add("page-item");
+     //here
+     if (i === currentPage) {
+      li.classList.add("active");
+    }
+    //to here
+    const link = document.createElement("a");
+    link.classList.add("page-link");
+    link.href = "#";
+    link.textContent = i;
+    li.appendChild(link);
+
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      currentPage = i;
+      filterProducts();
+      window.scrollTo(0, 0);
+      updateActivePage(); 
+    });
+
+    paginationElement.appendChild(li);
+  }
+}
+
+function attachAddToCartListeners() {
+  const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+  addToCartButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const productId = this.getAttribute("data-product-id");
+      addProductToCart(productId);
+      //console.log("Product added to cart:", productId);
+    });
+  });
+}
+
+// });
+
+
+function updateActivePage() {
+  const paginationItems = document.querySelectorAll(".pagination .page-item");
+
+  paginationItems.forEach((item, index) => {
+    if (index + 1 === currentPage) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+}
